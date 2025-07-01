f43d50612b211b44263a2242e0dbadfec91b9384
Madison-T <madisonlilah@gmail.com>
2025-07-01 13:56:33 +1200

    Fixed error in simple.tsx

--- src/pages/simple.tsx
+++ src/pages/simple.tsx
@@ -1,5 +1,14 @@
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
+import { useState } from "react";
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
 
 export default function Page() {
-  return <SimpleEditor />
+  const [content, setContent] = useState("");
+
+  return (
+    <div className="min-h-screen bg-[var(--background)] px-4 py-8">
+      <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
+        <SimpleEditor content={content} onUpdate={setContent} />
+      </div>
+    </div>
+  );
 }
